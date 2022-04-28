489ef12b82c6c9e4520197bc3b14b9df5ca2941d
Manjunath <sreerammanjunath1392@gmail.com>
2022-04-28 16:59:17 +0530

    COSTless

---
+++ static/js.js
@@ -0,0 +1,9 @@
+
+function Open(){
+    let Screen = document.getElementById('MobileScreenOpen');
+    Screen.style.width = '100vw'
+}
+function Close(){
+    let Screen = document.getElementById('MobileScreenOpen');
+    Screen.style.width = '0vw'
+}
